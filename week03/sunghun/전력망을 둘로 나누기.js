6ce65c3e93374c2851d9e777d5772fc6a7196f79
Sunghum Paik (Brian) <zerosial@gmail.com>
2025-08-18 09:31:51 +0900

    docs: 조건 및 found를 통해 결과를 찾으면 종료하게 변경

--- week03/sunghun/전력망을 둘로 나누기.js
+++ week03/sunghun/전력망을 둘로 나누기.js
@@ -1,27 +1,33 @@
 function solution(word) {
-  let w = "";
-  // let words = ["A", "E", "I", "O", "U"];
-  let words = ["A", "E"];
-  let wordsLimit = 5;
+  const V = ["A", "E", "I", "O", "U"];
+  const LIMIT = 5;
+
   let count = 0;
+  let found = false;
   let answer = 0;
-  function dfs(count) {
-    for (let i = 0; i < words.length; i++) {
-      if (w.length === wordsLimit) {
-        count++;
-        w.slice(0, -1);
+  let w = "";
+
+  function dfs() {
+    // 길이 1~5인 모든 접두어가 '단어'로 취급
+    if (w.length > 0) {
+      count++;
+      if (w === word) {
+        answer = count;
+        found = true;
+        return;
       }
-      if (count === wordsLimit) {
-        wordsLimit--;
-        count = 0;
-        continue;
-      }
-      w += words[count];
-      console.log(w);
-      dfs(count);
+    }
+    if (found) return;
+    if (w.length === LIMIT) return;
+
+    for (let i = 0; i < V.length; i++) {
+      if (found) return;
+      w += V[i];
+      dfs();
+      w = w.slice(0, -1);
     }
   }
 
-  dfs(count);
+  dfs();
   return answer;
 }
