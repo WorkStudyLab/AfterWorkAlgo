fdf2f3df41ce0653904573c73c9a05049a7216df
Sunghum Paik (Brian) <zerosial@gmail.com>
2025-08-18 11:39:16 +0900

    docs: 주석 추가

--- week03/sunghun/전력망을 둘로 나누기.js
+++ week03/sunghun/전력망을 둘로 나누기.js
@@ -16,6 +16,7 @@ function solution(n, wires) {
       if (!visited[v]) {
         const childSize = dfs(v);
         // 간선 (u, v)를 끊었다고 가정하면 한 쪽 크기가 childSize
+        // |childSize - (n - childSize)| 차이가 최소가 되는 경우를 찾는다.
         const diff = Math.abs(n - 2 * childSize);
         if (diff < answer) answer = diff;
 
